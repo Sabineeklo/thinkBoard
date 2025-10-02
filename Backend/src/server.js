ce64ab7450c2054741437617854bfd24911d7b37
Afi Sabine EKLO <afi-sabine.eklo@etu.univ-nantes.fr>
2025-10-02 03:27:19 +0200

    Add scricipt and configuration for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,22 +5,36 @@ import cors from 'cors';
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import ratelimiter from './middlware/rateLimiter.js';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // Middleware
+if (process.env.NODE_ENV !== 'production') {
 app.use(
   cors({
     origin: 'http://localhost:5173',
   })
 );
+}
+
+
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(ratelimiter);
 
 app.use('/api/notes', notesRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Frontend/dist', 'index.html'));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
